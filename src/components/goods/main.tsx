e4c83825ce6bb273e95e7777ffe0bdbef46cde4a
Poul Sunny <siesta.cloud@gmail.com>
2023-06-09 22:59:33 +0300

    ic

--- src/components/goods/main.tsx
+++ src/components/goods/main.tsx
@@ -1,8 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import { General } from "../pages/general/general";
-import { Category } from "../pages/category/category";
-import { Auth } from "../pages/auth/auth";
-import { Area } from "../pages/area/area";
 import { useAppState } from "../../hooks/appHook";
 
 
@@ -14,13 +11,6 @@ function Main() {
       <div className="main__wrapper">
         <Routes>
           <Route path={`/*`} element={<General />} />
-          <Route path={`/category/product/${12345}`} element={<Category />} />
-          <Route path={`/login`} element={<Auth togle={"login"} />} />
-          <Route path={`/register`} element={<Auth togle={"register"} />} />
-          <Route path={`/recover`} element={<Auth togle={"recover"} />} />
-          {AppState.areaSectionList.map((areaSection, i) =>
-            <Route path={`${areaSection.uri}`} element={<Area {...areaSection}/>} />
-          )}
         </Routes>
       </div>
     </main>
--- src/components/pages/general/general.tsx
+++ src/components/pages/general/general.tsx
@@ -1,20 +1,8 @@
-import { RowMenu, SubMenu } from "../../common/menu/menu";
-import { News } from "../../common/news/news";
-import { Sale } from "../../common/sale/sale";
-import { Trend } from "../../common/trend/trend";
 
 
 export function General() {
   return (
     <>
-      <div className="general__row">
-        <SubMenu />
-        <News />
-      </div>
-      <RowMenu />
-      <Trend />
-      <Sale />
-      <Trend />
     </>
   );
 }
